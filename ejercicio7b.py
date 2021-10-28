eb179d55bc9e5cbb6318fdef14ef26ef73ba8b86
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-10-28 19:51:14 +0200

    subida

--- ejercicio7b.py
+++ ejercicio7b.py
@@ -3,11 +3,15 @@ dinero = 2000
 edad = random.randrange(0,50)
 hambre = edad
 precio = 100
+total = 0
 
 while hambre < 85:
     if hambre > edad:
         precio = precio * 1.2
+        total = total + precio
+    else:
+        total += precio
     hambre = hambre + edad
 
 print(edad)
-print(precio)
+print(total)
